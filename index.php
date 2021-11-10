2f4ae5c54e163910877721cde90ddf7eb9ee8eec
Pichi <carrizoaagustin@gmail.com>
2021-11-10 14:15:22 -0300

    se crearon los modulos que componen la pagina web

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+  <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+  <!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+  <!--[if gt IE 8]><!-->
+  <html class="no-js" lang="">
+    <!--<![endif]-->
+    <?php   include("componentes/cabecera.html")  ?>
+
+    <body>
+      <!-- Left Panel -->
+      <?php include("componentes/leftPanel.html")?>
+      <!-- Left Panel -->
+
+      <!-- /#right-panel -->
+      <?php include("componentes/rightPanel.html")?>
+      <!-- /#right-panel -->
+
+      <?php include("componentes/script.html")?>
+    </body>
+  </html>
+</html>
